2547ad904a3aa01382915f1952ecb36727e69a76
Mrakshaymehta <135225242+Mrakshaymehta@users.noreply.github.com>
2025-04-09 15:12:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='us_visa',
     version='0.0.0',
-    author='Kaustubh Kaushik',
-    author_email='kaustubhkaushik26@gmail.com',
+    author='Akshay Mehta',
+    author_email='mrakshaymehta007@gmail.com',
     packages=find_packages(),
 )
